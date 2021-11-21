b17dbeaa8413798e556df6f3821ed158b8cb5106
Tyrel Chambers <tychambers3@gmail.com>
2021-11-21 15:42:11 +0000

    add link reddit route

--- api/user.js
+++ api/user.js
@@ -2,6 +2,7 @@ const express = require('express'***REMOVED***
 const authHandler = require('../middleware/authHandler'***REMOVED***
 const db = require('../models'***REMOVED***
 const app = express.Router()
+require('dotenv').config()
 
 app.get('/v1/me', authHandler, async (req, res, next) => {
   try {
@@ -17,6 +18,33 @@ app.get('/v1/me', authHandler, async (req, res, next) => {
     ***REMOVED******REMOVED***
 
     res.send(user***REMOVED*** 
+  ***REMOVED*** catch (error) {
+    next(error)
+  ***REMOVED***
+***REMOVED***)
+
+app.get('/v1/link-reddit', authHandler, async (req,res,next) => {
+  try {
+    const encode = window.btoa(
+      `${process.env.REDDIT_APP***REMOVED***:${process.env.REDDIT_SECRET***REMOVED***`
+    ***REMOVED***
+
+    const redditTokens = await Axios.post(
+      "https://www.reddit.com/api/v1/access_token",
+      `grant_type=authorization_code&code=${token***REMOVED***&redirect_uri=${process.env.REACT_APP_REDIRECT***REMOVED***/signup`,
+
+      {
+        headers: {
+          Authorization: `Basic ${encode***REMOVED***`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        ***REMOVED***,
+      ***REMOVED***
+    )
+      .then((res) => {
+        return res.data;
+      ***REMOVED***)
+      .catch(console.log)
+
   ***REMOVED*** catch (error) {
     next(error)
   ***REMOVED***
