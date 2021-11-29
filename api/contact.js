11e8def89469a007ca1121f200e599d2182fea09
Tyrel Chambers <tychambers3@gmail.com>
2021-11-29 00:15:12 +0000

    edit contact

--- api/contact.js
+++ api/contact.js
@@ -36,6 +36,47 @@ app.delete("/v1/delete/:uuid", authHandler, async (req, res, next) => {
   ***REMOVED***
 ***REMOVED******REMOVED***
 
+app.put("/v1/:id/edit", authHandler, async (req, res, next) => {
+  try {
+    const { id ***REMOVED*** = req.params;
+    const { name, notes ***REMOVED*** = req.body;
+
+    await db.Contact.update(
+      {
+        name,
+        notes,
+      ***REMOVED***,
+      {
+        where: {
+          uuid: id,
+          userId: res.locals.userId,
+        ***REMOVED***,
+      ***REMOVED***
+    ***REMOVED***
+
+    res.sendStatus(200***REMOVED***
+  ***REMOVED*** catch (error) {
+    next(error***REMOVED***
+  ***REMOVED***
+***REMOVED******REMOVED***
+
+app.get("/v1/:id", authHandler, async (req, res, next) => {
+  try {
+    const { id ***REMOVED*** = req.params;
+
+    const contact = await db.Contact.findOne({
+      where: {
+        uuid: id,
+        userId: res.locals.userId,
+      ***REMOVED***,
+    ***REMOVED******REMOVED***
+
+    res.send(contact***REMOVED***
+  ***REMOVED*** catch (error) {
+    next(error***REMOVED***
+  ***REMOVED***
+***REMOVED******REMOVED***
+
 app.get("/v1/", authHandler, async (req, res, next) => {
   try {
     const contacts = await db.Contact.findAll({
