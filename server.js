0666073d3527c9a1a24d43f177ad17d9df1358bb
Tyrel Chambers <tychambers3@gmail.com>
2022-01-02 11:39:39 -0500

    add upload endpoints

--- server.js
+++ server.js
@@ -9,21 +9,6 @@ const config = require("./config"***REMOVED***
 const mongoose = require("mongoose"***REMOVED***
 const app = express(***REMOVED***
 
-const auth = require("./api/auth"***REMOVED***
-const user = require("./api/user"***REMOVED***
-const posts = require("./api/posts"***REMOVED***
-const tokens = require("./api/token"***REMOVED***
-const contacts = require("./api/contact"***REMOVED***
-const contacted = require("./api/contacted"***REMOVED***
-const story = require("./api/story"***REMOVED***
-const inbox = require("./api/inbox"***REMOVED***
-const readingList = require("./api/readingList"***REMOVED***
-const tags = require("./api/tags"***REMOVED***
-const search = require("./api/search"***REMOVED***
-const website = require("./api/website"***REMOVED***
-const submitted = require("./api/submitted"***REMOVED***
-const subscriptions = require("./api/subscriptions"***REMOVED***
-
 // jobs
 const deleteCancelledSubscriptions = require("./jobs/deleteCancelledSubscriptions"***REMOVED***
 
@@ -55,21 +40,22 @@ mongoose.connect(database, { useNewUrlParser: true ***REMOVED******REMOVED***
 
 app.use(morgan("combined")***REMOVED***
 
-app.use("/api/auth", auth***REMOVED***
-app.use("/api/user", user***REMOVED***
-app.use("/api/posts", posts***REMOVED***
-app.use("/api/tokens", tokens***REMOVED***
-app.use("/api/contacts", contacts***REMOVED***
-app.use("/api/contacted", contacted***REMOVED***
-app.use("/api/story", story***REMOVED***
-app.use("/api/inbox", inbox***REMOVED***
-app.use("/api/reading_list", readingList***REMOVED***
-app.use("/api/tags", tags***REMOVED***
-app.use("/api/search", search***REMOVED***
-app.use("/api/website", website***REMOVED***
-app.use("/api/submitted", submitted***REMOVED***
-app.use("/api/subscriptions", subscriptions***REMOVED***
+app.use("/api/auth", require("./api/auth")***REMOVED***
+app.use("/api/user", require("./api/user")***REMOVED***
+app.use("/api/posts", require("./api/posts")***REMOVED***
+app.use("/api/tokens", require("./api/token")***REMOVED***
+app.use("/api/contacts", require("./api/contact")***REMOVED***
+app.use("/api/contacted", require("./api/contacted")***REMOVED***
+app.use("/api/story", require("./api/story")***REMOVED***
+app.use("/api/inbox", require("./api/inbox")***REMOVED***
+app.use("/api/reading_list", require("./api/readingList")***REMOVED***
+app.use("/api/tags", require("./api/tags")***REMOVED***
+app.use("/api/search", require("./api/search")***REMOVED***
+app.use("/api/website", require("./api/website")***REMOVED***
+app.use("/api/submitted", require("./api/submitted")***REMOVED***
+app.use("/api/subscriptions", require("./api/subscriptions")***REMOVED***
 app.use("/api/stripe", require("./api/stripe")***REMOVED***
+app.use("/api/upload", require("./api/upload")***REMOVED***
 
 db.on("error", console.error.bind(console, "Connection error - Mongodb")***REMOVED***
 db.once("open", () => console.log("Connected sucessfully to Mongo database")***REMOVED***
