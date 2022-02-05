7cd90b41a7ebeb184f61ab868708aa8f410824b2
Tyrel Chambers <tychambers3@gmail.com>
2022-02-05 17:53:52 +0000

    remove cache

--- api/announcements.js
+++ api/announcements.js
@@ -6,10 +6,6 @@ const StoryblokClient = require("storyblok-js-client");
 
 const Storyblok = new StoryblokClient({
   accessToken: process.env.STORYBLOK_TOKEN,
-  cache: {
-    clear: "auto",
-    type: "memory",
-  },
 });
 
 app.get("/v1/all", authHandler(), async (req, res, next) => {
