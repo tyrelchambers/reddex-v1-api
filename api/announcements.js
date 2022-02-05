64958d764b88ae0c544bd3c194d1e2154e99a7a9
Tyrel Chambers <tychambers3@gmail.com>
2022-02-05 16:05:43 +0000

    add announcement route

--- api/announcements.js
+++ api/announcements.js
@@ -1,11 +1,15 @@
 const { default: axios } = require('axios');
 const express = require('express');
-const {authHandler}=require('../middleware/authHandler')
+const authHandler=require('../middleware/authHandler')
 const app = express.Router()
 
 app.get('/v1/', authHandler, async (req, res, next) => {
   try {
-    const posts = await axios.get(`https://api.storyblok.com/v1/cdn/stories/home?version=published&token=${process.env.STORYBLOK_TOKEN}`)
+    console.log('here,--------------------');
+    // const posts = await axios.get(`https://api.storyblok.com/v1/cdn/stories/home?version=published&token=${process.env.STORYBLOK_TOKEN}`)
+    // console.log(posts);
+
+    res.sendStatus(200)
   } catch (error) {
     next(error)
   }
